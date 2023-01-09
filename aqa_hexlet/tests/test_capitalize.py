1918ab6819e6e23b0f0d330ed3f49b9e246dc048
Charlzzz <77102483+Charlzzz@users.noreply.github.com>
2023-01-10 00:13:04 +0700

    Delete aqa_hexlet directory

--- aqa_hexlet/tests/test_capitalize.py
+++
@@ -1,9 +0,0 @@
-from capitalize import capitalize
-
-assert capitalize('hello') != 'Hello':
-
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
-
-print('Все тесты пройдены!'
--- aqa_hexlet/tests/trash.py
+++
@@ -1,9 +0,0 @@
-def pig_it(text):
-    words = []
-    text = text.split()
-    for i in text:
-        print(words.append(i[1:]+i[0]+"ay"))
-    return " ".join(words)
-
-
-pig_it('Pig latin is cool')
